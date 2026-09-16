0080cd1261376c531eb7b61de5a6d1e3a3c6c05a
Huynh Thanh Dat <huynhthanhdat118@gmail.com>
2026-09-16 14:54:51 +0700

    feat: add pagination & sorting

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.StudentRequest;
@@ -35,8 +35,8 @@ public class StudentController {
 	
 		//Read
 	@GetMapping
-	public List<StudentResponse> getAllStudent(){
-		return studentService.getAllStudent();
+	public Page<StudentResponse> getAllStudent(@RequestParam int page, @RequestParam int size, @RequestParam String sort, @RequestParam String direction){
+		return studentService.getAllStudent(page, size, sort, direction);
 	}
 	
 	@GetMapping("/{id}")
