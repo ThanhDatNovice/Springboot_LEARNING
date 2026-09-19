c7ef6bd1c175973e89eccb1e5e2357469886032b
Huynh Thanh Dat <huynhthanhdat118@gmail.com>
2026-09-19 21:42:11 +0700

    feat: add JPQL Query and Filter

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,14 +36,36 @@ public class StudentController {
 	}
 	
 		//Read
+//	@GetMapping
+//	public Page<StudentResponse> getAllStudent(@RequestParam int page, @RequestParam int size, @RequestParam String sort, @RequestParam String direction){
+//		return studentService.getAllStudent(page, size, sort, direction);
+//	}
+	//New Version
 	@GetMapping
-	public Page<StudentResponse> getAllStudent(@RequestParam int page, @RequestParam int size, @RequestParam String sort, @RequestParam String direction){
-		return studentService.getAllStudent(page, size, sort, direction);
+	public Page<StudentResponse> getFilterStudents(
+			@RequestParam (required = false) String name, 
+			@RequestParam(required = false) Integer minAge, 
+			@RequestParam(required = false) Integer maxAge, 
+			@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "5") int size,
+			@RequestParam(defaultValue = "age") String Sort, 
+			@RequestParam(defaultValue = "asc") String direction){
+		return studentService.getAllStudent(name, minAge, maxAge, page, size, Sort, direction);
 	}
+	
 	
 	@GetMapping("/{id}")
 	public StudentResponse getIdStudent(@PathVariable Long id) {
 		return studentService.getIdStudent(id);
+	}
+	
+	@GetMapping("/age")
+	public List<StudentResponse> getAgeStudent(@RequestParam Integer age, @RequestParam String name){
+		return studentService.findByAge(age, name);
+	}
+	@GetMapping("/orderby")
+	public List<StudentResponse> getOrderbyAgeStudent(){
+		return studentService.findAllOrderby();
 	}
 	
 		//Update
